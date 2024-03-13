67f93a395db6afb5c325e2846bff090ff1fb82f6
yuji.tamai <yuji.tamai@medley.jp>
2024-03-13 21:09:00 +0900

    add

---
+++ 20240315/sample6.rb
@@ -0,0 +1,23 @@
+require 'js'
+require "cgi"
+
+def console_log(message)
+  puts "#{Time.now.strftime('%Y-%m-%d %H:%M:%S.%L')}: #{message}"
+end
+
+def output_html(message)
+  output = JS.global[:document].getElementById("output")
+  output[:innerHTML] = "#{output[:innerHTML]}#{CGI.escapeHTML(message)}<br>"
+end
+
+counter = 0
+render = ->() {
+  # clearIntervalの仕方がわからん
+  return if counter >= 5
+
+  console_log "loop #{counter}"
+  output_html "loop #{counter}"
+  counter += 1
+}
+
+JS.global.call(:setInterval, JS.try_convert(render), 1000)
