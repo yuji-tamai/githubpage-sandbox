d7a2e22a50d46a57fcc8ffb0d6b2603a444ebe80
yuji.tamai <yuji.tamai@medley.jp>
2024-03-13 20:29:40 +0900

    add

---
+++ 20240315/sample3.rb
@@ -0,0 +1,16 @@
+require 'js'
+require "cgi"
+
+def console_log(message)
+  puts "#{Time.now.strftime('%Y-%m-%d %H:%M:%S.%L')}: #{message}"
+end
+
+def output_html(message)
+  output = JS.global[:document].getElementById("output")
+  output[:innerHTML] = "#{output[:innerHTML]}#{CGI.escapeHTML(message)}<br>"
+end
+
+output_html "__FILE__ = [#{__FILE__.inspect}]"
+output_html "__dir__ = [#{__dir__.inspect}]"
+output_html "__LINE__ = [#{__LINE__.inspect}]"
+output_html "__ENCODING__ = [#{__ENCODING__.inspect}]"
