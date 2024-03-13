87c1a8fe053803e2752edc7a80a0a7b0dd520cff
yuji.tamai <yuji.tamai@medley.jp>
2024-03-13 19:59:57 +0900

    add sample

---
+++ 20240315/sample2.rb
@@ -0,0 +1,6 @@
+require 'js'
+
+now = Time.now.strftime('%Y-%m-%d %H:%M:%S.%L')
+puts "Hello, world! to console_log #{now}"
+output = JS.global[:document].getElementById("output")
+output[:innerHTML] = "Hello, world! to html #{now}"
