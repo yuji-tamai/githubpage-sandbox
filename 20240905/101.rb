1bdbcce6779b1625b4c9e4120c1e13b0d850d77f
tosiooooooo <tosiooooooo@gmail.com>
2024-09-01 22:13:55 +0900

    test

--- 20240905/101.rb
+++ 20240905/101.rb
@@ -11,15 +11,24 @@ def output_html(message)
   output[:innerHTML] = "#{output[:innerHTML]}#{CGI.escapeHTML(message)}<br>"
 end
 
-counter = 0
-render = ->() {
-  # clearIntervalの仕方がわからん
-  return if counter >= 100
+def execute
+  counter = 0
+  render = ->() {
+    # clearIntervalの仕方がわからん
+    return if counter >= 100
 
-  console_log "loop #{counter}"
-  output_html "loop #{counter}"
-  counter += 1
-}
+    console_log "loop #{counter}"
+    output_html "loop #{counter}"
+    counter += 1
+  }
 
-# JavascriptのsetIntervalにrenderのprocを渡す
-JS.global.call(:setInterval, JS.try_convert(render), 10)
+  # JavascriptのsetIntervalにrenderのprocを渡す
+  JS.global.call(:setInterval, JS.try_convert(render), 10)
+end
+
+button = document.getElementById('executeButton')
+
+button.addEventListener 'click' do |_e|
+  ruby_code = document.getElementById('rubyCode')[:value].to_s
+  execute_ruby_code(ruby_code)
+end
