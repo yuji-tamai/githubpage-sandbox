72d3e3e0143d5e0fdc9b59525f45b98ef6c7432d
tosiooooooo <tosiooooooo@gmail.com>
2024-09-01 22:25:09 +0900

    test

--- 20240905/101.rb
+++ 20240905/101.rb
@@ -6,13 +6,13 @@ def console_log(message)
   puts "#{Time.now.strftime('%Y-%m-%d %H:%M:%S.%L')}: #{message}"
 end
 
-def output_html(message)
-  output = JS.global[:document].getElementById("output")
-  output[:innerHTML] = "#{output[:innerHTML]}#{CGI.escapeHTML(message)}<br>"
-end
-
 def document
   @document ||= JS.global[:document]
+end
+
+def output_html(message)
+  output = document.getElementById("output")
+  output[:innerHTML] = "#{output[:innerHTML]}#{CGI.escapeHTML(message)}<br>"
 end
 
 def execute
