5bbb4fcbda28c29eb4c4f8cc440a14c9727679f4
tosiooooooo <tosiooooooo@gmail.com>
2024-09-02 22:01:35 +0900

    change

--- 20240905/101.rb
+++ 20240905/101.rb
@@ -15,62 +15,87 @@ def clear_html
   output[:innerHTML] = ""
 end
 
+$status = { omiai_ok: 0, karikousai_ok: 0, sinkenkousai_ok: 0, seikon_ok: 0 }
+
 # お見合い成立 6.3%
 # 仮交際成立 34.9%
 # 交際成立 9.6%
 # 結婚成立 59.8%
 def propose(counter)
+  height = "200px"
+  result = 5
   output = document.getElementById("output")
 
-  output[:innerHTML] = "#{counter}回目のプロポーズ: "
+  html = "#{counter} <img src='propose.png' height=50px /> "
   if rand(100) <= 6.3
-    output[:innerHTML] = "#{output[:innerHTML]}[1ok]"
+    html << "<img src='omiaiok.png' height=#{height} />"
+    $status[:omiai_ok] += 1
   else
-    output[:innerHTML] = "#{output[:innerHTML]}[1ng]<br>"
-    return false
+    html << "<img src='omiaing.png' height=#{height} />"
+    result = 1
   end
 
-  if rand(100) <= 34.0
-    output[:innerHTML] = "#{output[:innerHTML]}[2on]"
-  else
-    output[:innerHTML] = "#{output[:innerHTML]}[2ng]<br>"
-    return false
+  if result == 5
+    if rand(100) <= 34.0
+      html << "<img src='karikousaiok.png' height=#{height} />"
+      $status[:karikousai_ok] += 1
+    else
+      html << "<img src='karikousaing.png' height=#{height} />"
+      result = 2
+    end
   end
 
-  if rand(100) <= 9.6
-    output[:innerHTML] = "#{output[:innerHTML]}[3ok]"
-  else
-    output[:innerHTML] = "#{output[:innerHTML]}[3ng]<br>"
-    return false
+  if result == 5
+    if rand(100) <= 9.6
+      html << "<img src='sinkenkousaiok.png' height=#{height} />"
+      $status[:sinkenkousai_ok] += 1
+    else
+      html << "<img src='sinkenkousaing.png' height=#{height} />"
+      result = 3
+    end
   end
 
-  if rand(100) <= 59.8
-    output[:innerHTML] = "#{output[:innerHTML]}[4ok]<br>"
-  else
-    output[:innerHTML] = "#{output[:innerHTML]}[4ng]<br>"
-    return false
+  if result == 5
+    if rand(100) <= 59.8
+      html << "<img src='seikonok.png' height=#{height} />"
+      $status[:seikon_ok] += 1
+    else
+      html << "<img src='seikonng.png' height=#{height} />"
+      result = 4
+    end
   end
 
-  true
+  output[:innerHTML] = "#{html}<br>#{output[:innerHTML]}"
+  return result
 end
 
 def execute
   counter = 1
+  clear_html()
+  $status = { omiai_ok: 0, karikousai_ok: 0, sinkenkousai_ok: 0, seikon_ok: 0 }
+
   render = ->() {
     # clearIntervalの仕方がわからん
-    return if counter > 101
+    if counter > 101
+      if counter == 102
+        output = document.getElementById("output")
+        output[:innerHTML] = "#{$status.inspect}<br><br>#{output[:innerHTML]}"
+        counter += 1
+      end
+      return
+    end
 
-    clear_html()
-    console_log "loop #{counter}"
-    if propose(counter)
-      counter = 999
+    #console_log "loop #{counter}"
+    result = propose(counter)
+    if result == 5
+      counter = 102
     else
       counter += 1
     end
   }
 
   # JavascriptのsetIntervalにrenderのprocを渡す
-  JS.global.call(:setInterval, JS.try_convert(render), 100)
+  JS.global.call(:setInterval, JS.try_convert(render), 200)
 end
 
 button = document.getElementById('executeButton')
